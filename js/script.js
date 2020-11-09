576c77dec303ddb89dd7c15488daecffc3180c58
vladislav <vmirzoyev@yandex.ru>
2020-11-09 11:51:45 +0300

    JS-Task-1-1 1)Добавил класс 'second-page' элементу с классом 'page', затем удалил его в соответствии с тз 2) Стилизовал класс 'second-page', изменив фон

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.querySelector('.page');
+console.log(document.querySelector('.page'));
+document.querySelector('.page').classList.add('second-page');
+document.querySelector('.page').classList.remove('second-page');
