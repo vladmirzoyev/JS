842e75d323bff79ee6a4df2e78185039dae55448
vladislav <vmirzoyev@yandex.ru>
2020-11-15 21:42:16 +0300

    JS-Task-2-2 1) Добавил отступы между строками для улучшения читабельности кода 2) Добавил комментарии

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
+// Называем переменные
 let page = document.querySelector('.page');
 let lightButton = document.querySelector('.light-button');
 let message = document.querySelector('.message');
-console.log(document.querySelector('.page'));
-page.classList.add('second-page');
+
+console.log(document.querySelector('.page')); //Выводим класс с селектором .page
+page.classList.add('second-page'); //Добавляем новый класс
+//Функция: при нажатии на lightButton появляется сообщенеие об успешном нажатии
 lightButton.onclick = function () {
     console.log('Кнопка нажата!');
 };
+//Выводим текстовое значение из message в консоль
 console.log(message.textContent);
